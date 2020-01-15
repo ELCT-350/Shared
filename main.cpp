cf4756637330487f715669c9d6d9789a6957e109
Rod Leonard <leonard@cec.sc.edu>
2020-01-15 12:45:30 -0500

    added a hello world program

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream> //I/O classes (like cout)
+
+using namespace std;
+
+int main(int argc, char*argv[])
+{
+  cout << "Enter two numbers: ";
+  int a, b;
+  cin >> a >> b;
+
+  cout << "First number: " << a << endl
+       << "Second number: " << b << endl;
+
+  for(int i = 0; i < 10; ++i)
+  {
+  }
+
+  return 0;
+}
